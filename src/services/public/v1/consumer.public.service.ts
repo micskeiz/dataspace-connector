f524582c7537a5dbf72547f957db472bbc8a000e
Zoltan Micskei <micskeiz@mit.bme.hu>
2025-03-27 09:36:57 +0100

    Update consumer.public.service.ts

--- src/services/public/v1/consumer.public.service.ts
+++ src/services/public/v1/consumer.public.service.ts
@@ -147,7 +147,7 @@ export const triggerEcosystemFlow = async (props: {
 
     if (!purposeExists) {
         Logger.error({
-            message: 'Wrong purpose given ' + purposeId + '\n' + contractResponse.serviceOfferings,
+            message: 'Wrong purpose given ' + purposeId + '\n' + contractResponse.serviceOfferings[0],
             location: 'consumerExchange',
         });
         throw new ExchangeError(
